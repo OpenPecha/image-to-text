a296837b9aebec8ba7f0708f6732d4829d45ab26
gangagyatso4364 <gyatsokhan@gmail.com>
2024-02-07 16:13:33 +0530

    added test case

--- tests/test_rearrange.py
+++ tests/test_rearrange.py
@@ -4,23 +4,34 @@ from line_image_to_text.rearrange_input import load_json_data, process_work_fold
 def test_process_work_folder():
     process_work_folder("./tests/test_data/work")
     data = load_json_data("./tests/test_data/work/rearranged_work.json")
-    assert data[0]["rearranged_text"] == "ཁམས་སྒོམ་སྡེ་ནང་ཆེན་པའི་དགོན་ཁག་རྣམས་ཀྱི་"
-    assert data[1]["rearranged_text"] == "བྱུང་བ་ཕྱོགས་བསྒྲིགས་རིན་ཆེན་སྒྲོམ་བརྒྱ་"
-    assert (
-        data[2]["rearranged_text"]
-        == "ཝུའུ་ཧྲིང་ལོས།(ཁུལ་ཨུའི་རྒྱུན་ཨུ། རྫོང་ཨུ་ཧྲུའུ་ཅི)"
-    )
-    assert (
-        data[3]["rearranged_text"]
-        == "པད་མཆོག (རྫོང་དམངས་ཆེན་ཨུ་ཡོན་ལྷན་ཁང་གི་ཀྲུའུ་རིན）"
-    )
-    assert (
-        data[4]["rearranged_text"]
-        == "ཡོན་ཏན། (རྫོང་ཨུ་ཧྲུའུ་ཅི་གཞོན་པ། རྫོང་སྲིད་གཞུང་གི་རྫོང་གཙོ)"
-    )
-    assert (
-        data[5]["rearranged_text"]
-        == "ཀུན་དགའ་རྣམ་རྒྱལ། (རྫོང་ཨུ་རྒྱུན་ཨུ། སྒྲིག་ཨུ་ཧྲུའུ་ཅི)"
-    )
-    assert data[6]["rearranged_text"] == "ཨུ་ཡོན།"
-    assert data[7]["rearranged_text"] == "no matching text found"
+    for item in data:
+        if item["image_name"] == "I01JW1270005_0.jpg":
+            assert (
+                item["rearranged_text"] == "ཁམས་སྒོམ་སྡེ་ནང་ཆེན་པའི་དགོན་ཁག་རྣམས་ཀྱི་"
+            )
+        if item["image_name"] == "I01JW1270005_1.jpg":
+            assert item["rearranged_text"] == "བྱུང་བ་ཕྱོགས་བསྒྲིགས་རིན་ཆེན་སྒྲོམ་བརྒྱ་"
+        if item["image_name"] == "I01JW1270006_1.jpg":
+            assert (
+                item["rearranged_text"]
+                == "ཝུའུ་ཧྲིང་ལོས།(ཁུལ་ཨུའི་རྒྱུན་ཨུ། རྫོང་ཨུ་ཧྲུའུ་ཅི)"
+            )
+        if item["image_name"] == "I01JW1270006_2.jpg":
+            assert (
+                item["rearranged_text"]
+                == "པད་མཆོག (རྫོང་དམངས་ཆེན་ཨུ་ཡོན་ལྷན་ཁང་གི་ཀྲུའུ་རིན）"
+            )
+        if item["image_name"] == "I01JW1270006_3.jpg":
+            assert (
+                item["rearranged_text"]
+                == "ཡོན་ཏན། (རྫོང་ཨུ་ཧྲུའུ་ཅི་གཞོན་པ། རྫོང་སྲིད་གཞུང་གི་རྫོང་གཙོ)"
+            )
+        if item["image_name"] == "I01JW1270006_4.jpg":
+            assert (
+                item["rearranged_text"]
+                == "ཀུན་དགའ་རྣམ་རྒྱལ། (རྫོང་ཨུ་རྒྱུན་ཨུ། སྒྲིག་ཨུ་ཧྲུའུ་ཅི)"
+            )
+        if item["image_name"] == "I01JW1270006_12.jpg":
+            assert item["rearranged_text"] == "ཨུ་ཡོན།"
+        if item["image_name"] == "I1KG90740015_0.jpg":
+            assert item["rearranged_text"] == "no matching text found"
